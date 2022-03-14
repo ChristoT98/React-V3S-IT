96a69ac7ad8fec4c76cf968eb0fe8aecf9b11518
ChristoT98 <christolakshan@gmail.com>
2022-03-14 16:04:07 +0530

    Topbar Partially completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import About from "./components/about/About";
+import Services from "./components/services/Services";
+import Works from "./components/works/Works";
+import Contact from "./components/contact/Contact";
+import "./app.css";
+import { useState } from "react";
+import Menu from "./components/menu/Menu";
+
 function App() {
-  return <div className="app">hello</div>;
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <div className="app">
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <div className="sections">
+        <Intro />
+        <About />
+        <Services />
+        <Works />
+        <Contact />
+      </div>
+    </div>
+  );
 }
 
 export default App;
