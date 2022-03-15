6cc3b8bde8ffa98f1b9f8f5dd210d36466f1c98a
ChristoT98 <christolakshan@gmail.com>
2022-03-15 23:50:10 +0530

    fot awsome icon added

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,15 +1,28 @@
 import "./intro.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 export default function Intro() {
   return (
     <div className="intro" id="intro">
       <div className="content">
-        <h1>V3S-IT</h1>
-        <p>We build brands and beautiful experiences</p>
-        <div className="introBtn">
-          <a href="">More About Us</a>
-          <a href="">Let's Talk</a>
+        <h1 className="slideUp">V3S-IT</h1>
+        <h2 className="slideUp">We build brands and beautiful experiences</h2>
+        <div className="introBtnContainer slideUp">
+            <a href="" className="introBtnAbout btn">
+              More About Us
+            </a>
+            <a href="" className="introBtnContact btn">
+              Let's Talk
+            </a>
         </div>
+        <h5 style={{color: 'black'}}>Heyyyy</h5>
+        <div className="socialIcons">
+       <ul><li><a href=""><FontAwesomeIcon icon={faCoffee} /></a></li></ul>
+        </div>
+      </div>
+      <div>
+     
       </div>
     </div>
   );
