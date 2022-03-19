4f7e9881ad62a1bc9890c267c50ec693f0a9eea0
ChristoT98 <christolakshan@gmail.com>
2022-03-19 10:40:32 +0530

    Footer section Completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,18 @@
 import "./footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footerbtnContainer">
+        <a href="#intro" className="footerbtnText">
+          <FontAwesomeIcon icon={faArrowUp} className="btnIcon" /> Back To Top
+        </a>
+      </div>
+      <div className="copyrightContainer">
+        <span className="copyrightText">© Copyright v3s-IT 2022</span>
+      </div>
+    </div>
+  );
 }
