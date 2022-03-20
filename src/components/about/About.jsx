6ec5c21d0a2dae56c24ef50b887359753444388e
ChristoT98 <christolakshan@gmail.com>
2022-03-20 12:39:39 +0530

    About section completed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,68 @@ import "./about.css";
 export default function About() {
   return (
     <div className="about" id="about">
-      About
+      <div className="aboutContentContainer">
+        <h3 className="aboutSubText">Who We Are</h3>
+        <h1 className="aboutHeadText">
+          We are v3s-IT, a design and branding agency with partners worldwide.
+          We design thoughtful digital experiences and beautiful brand
+          aesthetics.
+        </h1>
+        <h2 className="aboutDescriptionText">
+          It was a bow, a layer of this financing and, ferry at sem. Until the
+          makeup of the lion needs Uncategorized The pain itself is love, the
+          main iterative process, but I give it time as low as fatigue and
+          obesity. For more information on what we do the school district's
+          employment is only to achieve its objectives. The pain is to blame in
+          he wants pleasure to be.
+        </h2>
+      </div>
+      <div className="aboutGridContainer">
+        <div className="gridBox">
+          <h2 className="gridBoxTitle">
+            <span className="gridBoxNumber"> 1 </span> Define
+          </h2>
+          <p className="gridBoxContent">
+            Quos dolores saepe mollitia deserunt accusamus autem reprehenderit.
+            Voluptas facere animi explicabo non quis magni recusandae. Numquam
+            debitis pariatur omnis facere unde. Laboriosam minus amet nesciunt
+            est. Et saepe eos maxime tempore quasi deserunt ab.
+          </p>
+        </div>
+        <div className="gridBox">
+          <h2 className="gridBoxTitle">
+            <span className="gridBoxNumber"> 2 </span> Design
+          </h2>
+          <p className="gridBoxContent">
+            Quos dolores saepe mollitia deserunt accusamus autem reprehenderit.
+            Voluptas facere animi explicabo non quis magni recusandae. Numquam
+            debitis pariatur omnis facere unde. Laboriosam minus amet nesciunt
+            est. Et saepe eos maxime tempore quasi deserunt ab.
+          </p>
+        </div>
+        <div className="gridBox">
+          <h2 className="gridBoxTitle">
+            <span className="gridBoxNumber"> 3 </span> Build
+          </h2>
+          <p className="gridBoxContent">
+            Quos dolores saepe mollitia deserunt accusamus autem reprehenderit.
+            Voluptas facere animi explicabo non quis magni recusandae. Numquam
+            debitis pariatur omnis facere unde. Laboriosam minus amet nesciunt
+            est. Et saepe eos maxime tempore quasi deserunt ab.
+          </p>
+        </div>
+        <div className="gridBox">
+          <h2 className="gridBoxTitle">
+            <span className="gridBoxNumber"> 4 </span> Launch
+          </h2>
+          <p className="gridBoxContent">
+            Quos dolores saepe mollitia deserunt accusamus autem reprehenderit.
+            Voluptas facere animi explicabo non quis magni recusandae. Numquam
+            debitis pariatur omnis facere unde. Laboriosam minus amet nesciunt
+            est. Et saepe eos maxime tempore quasi deserunt ab.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
